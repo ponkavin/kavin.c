dfeee41c4fb7a78dd01a54ee03094a3da28fbb0d
ponkavin <35687942+ponkavin@users.noreply.github.com>
2018-02-02 14:18:57 +0530

    kavin

---
+++ k1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if((a%2)=0)
+printf("even");
+elseprintf("odd");
+getch();
+return 0;
+}
