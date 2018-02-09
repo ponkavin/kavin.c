035316a5ee6acea1daf354bdb46cc26403bcdc51
ponkavin <35687942+ponkavin@users.noreply.github.com>
2018-02-09 13:33:37 +0530

    Create alpha(feb9).C

---
+++ alpha(feb9).C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   char c;
+   printf("enter the character");
+   scanf("%c",&c);
+   if(c>='a' && c<='z')
+{
+   printf("%c alphabet",c);
+}
+   else
+{
+   printf("%c not alphabet",c);
+ getch();
+ } 
