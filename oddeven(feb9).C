ad00e0aa7d2d4353c5ac323d13defb49256d8816
ponkavin <35687942+ponkavin@users.noreply.github.com>
2018-02-09 13:24:15 +0530

    Update oddeven(feb9).C

--- oddeven(feb9).C
+++ oddeven(feb9).C
@@ -1,14 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
-int a;
-clrscr();
-printf("enter the number");
-scanf("%d",&a);
-if((a%2)=0)
-printf("even");
-elseprintf("odd");
-getch();
-return 0;
+   int a;
+   clrscr();
+   printf("enter the number");
+   scanf("%d",&a);
+   if((a%2)=0)
+   printf("even");
+   elseprintf("odd");
+   getch();
+   return 0;
 }
