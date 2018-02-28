be49bdb25425d1a558cc96e253790d1a84c8f683
ponkavin <35687942+ponkavin@users.noreply.github.com>
2018-02-28 16:03:33 +0530

    Update prin(feb28).C

--- prin(feb28).C
+++ prin(feb28).C
@@ -1,32 +1,32 @@
 #include<stdio.h>
 int main()
 {
-	int a[10],i,j,n,temp;
-	printf("enter the number");
-	scanf("%d",&n);
-	printf("list of element");
-	for(i=0;i<n;i++)
+	   int a[10],i,j,n,temp;
+	   printf("enter the number");
+	   scanf("%d",&n);
+	   printf("list of element");
+	   for(i=0;i<n;i++)
+{
+	     scanf("%d",&a[i]);
+}
+	     for(i=0;i<n-1;i++)
 	{
-	    scanf("%d",&a[i]);
-	}
-	for(i=0;i<n-1;i++)
-	{
-	    for(j=i+1;j<n;j++)
+	       for(j=i+1;j<n;j++)
 	    {
-	        if(a[i]>a[j])
+	          if(a[i]>a[j])
 	        {
-	        temp=a[i];
-	        a[i]=a[j];
-	        a[j]=temp;
+	          temp=a[i];
+	          a[i]=a[j];
+	          a[j]=temp;
 	        }
 	}
 	}
-	printf("sorted list \n");
-	for(i=0;i<n;i++)
+	  printf("sorted list \n");
+	  for(i=0;i<n;i++)
 	{
-	    printf("%d\n",a[i]);
+	      printf("%d\n",a[i]);
 	}
-	printf("minimum number is:");
-	printf("%d\n",a[0]);
-	return 0;
+	  printf("minimum number is:");
+	  printf("%d\n",a[0]);
+	  return 0;
 }
