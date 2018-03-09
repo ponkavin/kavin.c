0566d35c13f0c4e16a86ec66a084fa9575b7b1da
ponkavin <35687942+ponkavin@users.noreply.github.com>
2018-03-09 13:17:19 +0530

    Rename countchar(mar7).C to countnum(mar7).C

---
+++ countnum(mar7).C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+     char a[20];
+     int i,count=1,j;
+     gets(a);
+     j=strlen(a);
+     for(i=0;i<=c;i++)
+    {
+      if(a[i]==' ')
+    {
+       count=count+1;
+    }
+    }
+     printf("the total words is %d",count);
+}
